96c608525087348f94c3a60fa4a476b19bdce9c1
Israel-Silvestre <israelvaz2705@gmail.com>
2024-12-06 11:11:12 -0300

    feat: exemplo html

--- lib/Tabs_Screens/code_cards.dart
+++ lib/Tabs_Screens/code_cards.dart
@@ -1,19 +1,31 @@
 import 'package:flutter/material.dart';
 
+import '../HTML/interpreter/html_interpreter.dart';
+
 class CodeListView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
-      itemCount: 1,
+      itemCount: 2,
       itemBuilder: (context, index) {
-        return Container(
-          margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-          height: 200, // Altura do card
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(10), // Bordas arredondadas
-            child: Image.asset(
-              'assets/code_cards/card${index + 1}.png',
-              fit: BoxFit.fill, // Imagem cobre o card inteiro
+        return GestureDetector(
+          onTap: () {
+            if (index == 1) {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => HtmlEditor()),
+              );
+            }
+          },
+          child: Container(
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            height: 200, // Altura do card
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10), // Bordas arredondadas
+              child: Image.asset(
+                'assets/code_cards/card${index + 1}.png',
+                fit: BoxFit.fill, // Imagem cobre o card inteiro
+              ),
             ),
           ),
         );
